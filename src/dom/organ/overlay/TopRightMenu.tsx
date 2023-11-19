07b5bb5272eeddb02719c7dcc9d6a1a76254109b
Abraham Duno <abraham.d@servicepad.com>
2023-11-19 10:01:40 -0400

    g

--- src/dom/organ/overlay/TopRightMenu.tsx
+++ src/dom/organ/overlay/TopRightMenu.tsx
@@ -7,33 +7,34 @@ export default function TopRightMenu({...props}) {
     return <>
         <div className="flex-row gap-3 Q_xs_sm_flex-col">
             
-            <Link href={"https://webpov.vercel.app/"} className=" opaci-chov--50 Q_xs">
+            <Link href={"https://webpov.vercel.app/"} className=" opaci-chov--50 Q_xs" title="WebPOV">
               <div className="tx-lx bg-white pa-1 pb-0 pt-2 bord-r-25" >
               <Image className="box-shadow-5-b bord-r-100p noverflow" alt="asd" src="/webpovlogo.jpg" width={64} height={64} />
               </div>
               </Link>
-                <a href={"/"} className=" opaci-chov--50">
-              <div className="tx-lx bg-w-10 border-white pa-1 pb-0 pt-2 bord-r-25" >
-              <Image className="box-shadow-5-b bord-r-100p noverflow" alt="asd" src="/wequb.png" width={48} height={48} />
+                <a href={"/"} className=" opaci-chov--50" title="WeGame">
+              <div className="tx-lx bg-w-10  pa-1 pb-0 pt-2 bord-r-25" >
+              <Image style={{border:"2px dotted white"}}
+                 className="box-shadow-5-b bord-r-100p noverflow border-" alt="asd" src="/wegame2.jpg" width={48} height={48} />
               </div>
               </a>
-                <Link href={"https://webqub.vercel.app/"} className=" opaci-chov--50">
+                <Link href={"https://webqub.vercel.app/"} className=" opaci-chov--50" title="WebQub">
               <div className="tx-lx bg-white pa-1 pb-0 pt-2 bord-r-25" >
               <Image className="box-shadow-5-b bord-r-100p noverflow" alt="asd" src="/webqublogo.jpg" width={48} height={48} />
               </div>
               </Link>
-                <Link href={"https://wcity.vercel.app/"} className=" opaci-chov--50">
+                <Link href={"https://wcity.vercel.app/"} className=" opaci-chov--50" title="WebCity">
               <div className="tx-lx bg-white pa-1 pb-0 pt-2 bord-r-25" >
-              <Image className="box-shadow-5-b bord-r-100p noverflow" alt="asd" src="/webcitylogo.jpg" width={48} height={48} />
+              <Image className="box-shadow-5-b bord-r-100p noverflow" alt="asd" src="/webc.jpg" width={48} height={48} />
               </div>
               </Link>
-                <Link href={"https://wcity.vercel.app/w"} className=" opaci-chov--50">
+                <Link href={"https://wcity.vercel.app/w"} className=" opaci-chov--50" title="WebTown">
               <div className="tx-lx bg-white pa-1 pb-0 pt-2 bord-r-25" >
-              <Image className="box-shadow-5-b bord-r-100p noverflow" alt="asd" src="/webgamelogo.jpg" width={48} height={48} />
+              <Image className="box-shadow-5-b bord-r-100p noverflow" alt="asd" src="/webt.jpg" width={48} height={48} />
               </div>
               </Link>
               </div>
-                <Link href={"https://webpov.vercel.app/"} className=" opaci-chov--50 Q_sm_x">
+                <Link href={"https://webpov.vercel.app/"} className=" opaci-chov--50 Q_sm_x" title="WebPOV">
               <div className="tx-lx bg-white pa-1 pb-0 pt-2 bord-r-25" >
               <Image className="box-shadow-5-b bord-r-100p noverflow" alt="asd" src="/webpovlogo.jpg" width={64} height={64} />
               </div>
--- src/dom/organ/stage/HomeStage.tsx
+++ src/dom/organ/stage/HomeStage.tsx
@@ -1,6 +1,7 @@
 "use client"
 import Link from "next/link";
 import { ReactNode, useState } from "react";
+import TopRightMenu from "../overlay/TopRightMenu";
 
 const defaultGameCats = [
     // "world",
@@ -12,6 +13,7 @@ export type GameCat = {
     href:string
     name:string
     color:string
+    desc?:string
     emoji:string
     disabled:boolean
 }
@@ -40,10 +42,11 @@ const defaultGameCatsLookup:Record<string, GameCat> = {
     },
     "examples": {
         href: "/x",
-        name: "Categories",
+        name: "Game Examples",
         color:"#00ff00",
         emoji: "🔠",
         disabled: false,
+        desc:"Game Types & Forms of Play by Data Muatability and Refresh Rate",
     },
 }
 
@@ -67,7 +70,7 @@ export default function HomeStage({children}:{children:ReactNode}) {
         s__loadingCat(categoryString)
     }
     return (<>
-        <div className="tx-white flex-wrap w-100 tx-altfont-1 gap-1 tx-lx py-8 ">
+        <div className="tx-white flex-col gap-4 w-100 tx-altfont-1 gap-1 tx-lx py-8 ">
             {!!loadingCat && <>
                 <div className="flex-col">
                     <div className="tx-lg opaci-50">Loading {loadingCat}...</div>
@@ -78,19 +81,23 @@ export default function HomeStage({children}:{children:ReactNode}) {
                 const lookupGameCat = defaultGameCatsLookup[aGameCat]
                 return (<div key={index} className="w-30">
                     <div  
-                        className={`h-250px  tx-white opaci-hov-75 bord-r-25  nodeco  flex-col flex-justify-start pos-rel ${!lookupGameCat.disabled ? "bg-w-20" : "bg-w-10"}`}
+                        className={`h-250px pt-2 tx-white opaci-hov-75 bord-r-25  nodeco  flex-col flex-justify-start pos-rel ${!lookupGameCat.disabled ? "bg-w-20" : "bg-w-10"}`}
                     >
-                        <div className="Q_xs_md tx-mdl pt-4 ">{aGameCat.toUpperCase()}</div>
-                        <div className="Q_md_lg tx-xl">{aGameCat}</div>
-                        <div className="Q_xl_x tx-xxxl">{aGameCat}</div>
+                        {!!lookupGameCat.name && <>
+                          <div className="Q_xs_md tx-mdl pt-4 ">{lookupGameCat.name.toUpperCase()}</div>
+                          <div className="Q_md_lg tx-xl">{lookupGameCat.name}</div>
+                          <div className="Q_xl_x tx-xxxl">{lookupGameCat.name}</div>
+                        </>}
 
-                        <div className=" tx-bold-8 opaci-50 tx-ls-2 tx-center pt-1">
-                            <div className="Q_xs tx-xsm ">{lookupGameCat.name}</div>
-                            <div className="Q_sm_x tx-mdl">{lookupGameCat.name.toUpperCase()}</div>
-                        </div>
+                        {!!lookupGameCat.desc && <>
+                          <div className=" tx-bold-8 opaci-50 tx-ls-2 tx-center pa-3">
+                              <div className="Q_xs tx-xsm ">{lookupGameCat.desc}</div>
+                              <div className="Q_sm_x tx-mdl">{lookupGameCat.desc.toUpperCase()}</div>
+                          </div>
+                        </>}
                         <Link href={lookupGameCat.disabled ? "/x" : lookupGameCat.href} 
                             onClick={()=>triggerTypeClick(aGameCat)}
-                            className="bg-w-10 Q_xs_px-2  tx-white tx-altfont-4 tx-shadow-5 tx-bold-8 flex-center opaci-chov--50 box-shadow-5-b nodeco opaci-chov-50 bord-r-50 w-50 pa-5 pos-abs top-50p mb-4" 
+                            className="bg-w-10 Q_xs_px-2  tx-white tx-altfont-4 tx-shadow-5 tx-bold-8 flex-center opaci-chov--50 box-shadow-5-b nodeco opaci-chov-50 bord-r-50 w-50 pa-5 py-2 pos-abs bottom-0 mb-6 " 
                             style={{
                                 border:`1px solid ${lookupGameCat.color}`,
                                 color:lookupGameCat.color,
@@ -98,11 +105,14 @@ export default function HomeStage({children}:{children:ReactNode}) {
                             }}
                         >
                             <div className="Q_lg_x tx-lg">Enter</div>
-                            <div>{lookupGameCat.emoji}</div>
+                            {/* <div>{lookupGameCat.emoji}</div> */}
                         </Link>
                     </div>
                 </div>)
             })}
+            <hr className="w-50 opaci-10" />
+            <div>WebPOV Apps</div>
+            <TopRightMenu />
         </div>
     </>)
 }
